c7621ee9c42461a8a59618a84b3c1341dc15e040
Aaron Tuggle <aarontuggle32900@gmail.com>
2024-01-13 16:45:38 -0700

    Compleated prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //define variables
+        string first;
+        string last;
+        //ask for and get information
+        Console.Write("What is your first name? ");
+        first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        last = Console.ReadLine();
+        //display results
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+
     }
 }
