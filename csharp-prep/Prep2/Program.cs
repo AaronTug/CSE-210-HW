0f790825f342ac51167320d607a3a6504696db56
Aaron Tuggle <aarontuggle32900@gmail.com>
2024-01-13 17:07:12 -0700

    compleated prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string wordGrade = Console.ReadLine();
+        int grade = int.Parse(wordGrade);
+        string letter;
+        if (grade < 60)
+        {
+            letter = "F";
+        }
+        else if (grade >= 80)
+        {
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+            else
+            {
+                letter = "B";
+            }
+        }
+        else
+        {
+            if (grade >= 70)
+            {
+                letter = "C";
+            }
+            else
+            {
+                letter = "D";
+            }
+        }
+        Console.WriteLine($"Your letter grade is {letter}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congradulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Try again.");
+        }
+
     }
 }
