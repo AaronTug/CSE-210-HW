3b720f33ee834476dc7cb6b39d7374f2155d5278
Aaron Tuggle <aarontuggle32900@gmail.com>
2024-01-17 17:35:05 -0700

    compleated prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //define variables
+        int guess = -1;
+        int random;
+        //get random number
+        Random randomgenerator = new Random();
+        random = randomgenerator.Next(1, 101);
+        do
+        {
+            //ask for user to guess
+            Console.Write("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            //if higher
+            if (guess > random)
+            {
+                Console.WriteLine("Your Guess is to High");
+            }
+
+            //if lower
+            else if (guess < random)
+            {
+                Console.WriteLine("Your Guess is to Low");
+            }
+
+            //if correct
+            else
+            {
+                Console.WriteLine("You Got It!!");
+            }
+        } while (guess != random);
     }
 }
