5299f2add99f1785a1cfd58ee13ea98848022cce
Aaron Tuggle <aarontuggle32900@gmail.com>
2024-01-19 21:40:23 -0700

    compleated prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Data;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> form = new List<int>();
+        int num;
+        int sum = 0;
+        int count;
+        int average;
+        int largest = 0;
+        Console.WriteLine("Please enter a number to add to the form, add 0 when done.");
+
+        do
+        {
+            string inter;
+            Console.Write("Enter number: ");
+            inter = Console.ReadLine();
+            num = int.Parse(inter);
+            form.Add(num);
+        } while (num != 0);
+
+        foreach (int numb in form)
+        {
+            sum = sum + numb;
+            if (numb > largest)
+            {
+                largest = numb;
+            }
+        }
+        count = form.Count - 1;
+        average = sum / count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+
+
     }
 }
