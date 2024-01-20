ed944f540867fb95066165601d72e4b16e6ee818
Aaron Tuggle <aarontuggle32900@gmail.com>
2024-01-20 11:55:47 -0700

    Compleated prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string name;
+        int num;
+        double square;
+        Program p = new Program();
+        p.DisplayWelcome();
+        name = p.PromtUserName();
+        num = p.PromptUserNumber();
+        square = p.SquareNumber(num);
+        p.DisplayResults(name, square);
+    }
+
+    void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    string PromtUserName()
+    {
+        string name;
+        Console.Write("Please enter your name: ");
+        name = Console.ReadLine();
+        return (name);
+    }
+
+    int PromptUserNumber()
+    {
+        int num;
+        Console.Write("Please enter your favorite number: ");
+        num = int.Parse(Console.ReadLine());
+        return (num);
+    }
+
+    double SquareNumber(int num)
+    {
+        double result = Math.Pow(num, 2);
+        return (result);
+    }
+
+    void DisplayResults(string name, double square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}.");
     }
 }
