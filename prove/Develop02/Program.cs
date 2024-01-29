ebb464efaa54c18326905737a149c0c0a0a02e3c
Aaron Tuggle <aarontuggle32900@gmail.com>
2024-01-29 11:09:06 -0700

    saving untill i get over brain fog

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,8 +7,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        Program p = new Program();
         Console.WriteLine("Hello Develop02 World!");
         Console.Clear();
+        p.showMenu();
+
+    }
+
+    int showMenu()
+    {
+        int responce = int.Parse(Console.ReadLine());
+        return (responce);
 
     }
 
