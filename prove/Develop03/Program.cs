10fa040e8db2b1796c5ddc6066825d227a690d3a
Aaron Tuggle <aarontuggle32900@gmail.com>
2024-02-13 15:00:09 -0700

    update

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Xml.Serialization;
 
 class Program
 {
+    private int selection;
+    private string SripRef;
+
+    Scripture choice = new Scripture();
+    Reference thisone = new Reference();
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Program p = new Program();
+        p.giveOptions();
+        p.GiveScripture();
+        p.printReference(SripRef);
+        p.printScripture();
     }
+
+    void giveOptions()
+    {
+        Console.WriteLine("Please Select the Scripture you would like to practice");
+        Console.WriteLine("1-- 1st Nephi 3:7");
+        Console.WriteLine("2-- D&C 8: 2-3");
+        selection = int.Parse(Console.ReadLine());
+    }
+
+    void GiveScripture()
+    {
+        SripRef = thisone.getReference(selection);
+
+    }
+
+    void printReference(string Ref)
+    {
+        Console.WriteLine($"<{Ref}>");
+
+    }
+
+    void printScripture()
+    {
+
+    }
+
 }
--- prove/Develop03/Scriptures.cs
+++ prove/Develop03/Scriptures.cs
@@ -1,9 +1,11 @@
 class Scripture
 {
+    private Reference thisone = new Reference();
     private List<string> verses = new List<string>();
 
     public string GiveScripture(int i)
     {
+
         return ("placeholder");
     }
 
