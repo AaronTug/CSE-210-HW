4c53d73c9020077fc210a2c92a525784f7952fe2
Aaron Tuggle <aarontuggle32900@gmail.com>
2024-03-25 10:26:31 -0600

    update

---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,13 @@
+class Video
+{
+    private string title;
+    private string author;
+    private int legnth;
+
+    public Video(string Title, string Author, int Legnth)
+    {
+        this.title = Title;
+        this.author = Author;
+        this.legnth = Legnth;
+    }
+}
